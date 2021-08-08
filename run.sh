c4a8ced2b7eb9cf5790d5e90eb575cfd1efb1d2b
realwzc <1014369633@qq.com>
2021-08-08 20:36:32 +0800

    delete mk.sh run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-bsub -b -I -q q_sw_expr -n 56 -np 4 -cgsp 64 -host_stack 256 -share_size 4096 ./swtest 10000 200000 2 0.125 1000 5.5
-#./swtest connectnumber neuronnumber maxdelay timestep simluatetime currentfactor
